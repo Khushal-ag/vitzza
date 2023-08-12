70ac30445462024fb6aeada0faa744a2a21e48d7
Khushal Agarwal <starkhush5@gmail.com>
2023-08-12 23:49:40 +0530

    fix:store imports

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import cartReducer from "../features/cartSlice";
-import userReducer from "../features/userSlice";
+import cartReducer from "./features/cart/cartSlice";
+import userReducer from "./features/user/userSlice";
 
 const store = configureStore({
   reducer: {
