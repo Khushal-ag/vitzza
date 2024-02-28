222935fd405c5196a771886d251340cc940012a6
Khushal Agarwal <starkhush5@gmail.com>
2024-02-28 09:48:49 +0530

    chore: renovate.json configured
    Signed-off-by: Khushal Agarwal <starkhush5@gmail.com>

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -32,14 +32,18 @@ function CreateOrder() {
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
-      <Form method="POST">
+      <form>
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
+          <input className="input grow" type="text" name="customer" required />
+        </div>
+
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Email</label>
           <input
             className="input grow"
-            type="text"
-            name="customer"
-            defaultValue={username}
+            type="email"
+            name="customer-email"
             required
           />
         </div>
@@ -48,11 +52,6 @@ function CreateOrder() {
           <label className="sm:basis-40">Phone number</label>
           <div className="grow">
             <input className="input w-full" type="tel" name="phone" required />
-            {formErrors?.phone && (
-              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
-                {formErrors.phone}
-              </p>
-            )}
           </div>
         </div>
 
@@ -63,65 +62,19 @@ function CreateOrder() {
               className="input w-full"
               type="text"
               name="address"
-              defaultValue={address}
-              disabled={isAddressLoading}
               required
             />
-            {addressStatus === "error" && (
-              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
-                {errorAddress}
-              </p>
-            )}
           </div>
-          {!position.latitude && !position.longitude && (
-            <span className="absolute right-[3px] top-[3px] z-50 md:right-[5px] md:top-[5px] ">
-              <Button
-                disabled={isAddressLoading}
-                type="small"
-                onClick={(e) => {
-                  e.preventDefault();
-                  dispatch(fetchAddress());
-                }}
-              >
-                Get Position
-              </Button>
-            </span>
-          )}
-        </div>
-
-        <div className="mb-12 flex items-center gap-5">
-          <input
-            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
-            type="checkbox"
-            name="priority"
-            id="priority"
-            value={withPriority}
-            onChange={(e) => setWithPriority(e.target.checked)}
-          />
-          <label htmlFor="priority" className="font-medium">
-            Want to yo give your order priority?
-          </label>
         </div>
 
         <div>
-          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <input
-            type="hidden"
-            name="position"
-            value={
-              position.latitude && position.longitude
-                ? `${position.latitude},${position.longitude}`
-                : ""
-            }
-          />
-
-          <Button disabled={isSubmitting || isAddressLoading} type="primary">
+          <Button type="primary">
             {isSubmitting
               ? "Placing Order..."
               : `Order now for ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
-      </Form>
+      </form>
     </div>
   );
 }
